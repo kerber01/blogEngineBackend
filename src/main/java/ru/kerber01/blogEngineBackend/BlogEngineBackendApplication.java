9275b1fc03b1b55349f57d1c3108252e2abcea38
kerber <raman.fernanda@gmail.com>
2020-09-04 22:24:05 +0300

    Spring application created

---
+++ src/main/java/ru/kerber01/blogEngineBackend/BlogEngineBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.kerber01.blogEngineBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEngineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogEngineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kerber01/blogEngineBackend/BlogEngineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kerber01.blogEngineBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEngineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
